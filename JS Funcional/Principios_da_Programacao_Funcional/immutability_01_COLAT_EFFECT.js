7a04be2178cfb2192b656c7d25f8c473049d9a94
Teo Nogueira <teonogueira17@gmail.com>
2021-07-23 20:08:55 -0300

    slice

--- JS Funcional/Principios_da_Programacao_Funcional/immutability_01_COLAT_EFFECT.js
+++ JS Funcional/Principios_da_Programacao_Funcional/immutability_01_COLAT_EFFECT.js
@@ -11,7 +11,7 @@ return [...array].sort((a, b) =>  a + b) //[...array] função pura gerando clon
 
 const nums = [3, 1, 7, 9, 4, 6]
 
-// nums[3] = 1000
+// nums[3] = 1000 se eu colocar esse valor ele continua funcionando e irá mudar os dados internos pois é alterável, utilize  o Object.freeze() pra congelar o array.
 
 const numsOrder = order(nums)
 
@@ -19,7 +19,7 @@ const numsOrder = order(nums)
 
  /// SLICE TIRAR PARTE DO ARRAY MESMO ASSIM NÃO ALTERA POIS O ARRAY ESTÁ INTACTO
 
-const parteNums = nums.slice(2)
+const parteNums = nums.slice(2) // tirar dois valores do array, repare que o array original continua imune
 
 
 console.log(parteNums, nums)
