9ce3b86b568cfa10ec942953093bfd1d5dd4a9f5
Teo Nogueira <teonogueira17@gmail.com>
2021-11-03 09:43:05 -0300

    training javascript

--- JS Funcional/NoStopJavaScript/booksbox2.js
+++ JS Funcional/NoStopJavaScript/booksbox2.js
@@ -14,6 +14,7 @@ if(booksBox.booksIn - booksBox.spaces) {
    
 const singularOrPluralHa = booksBox.booksIn === 1 ? 'Existe' : 'Há'
 const singularOrPluralBooks = booksBox.booksIn === 1 ? 'Livro' : 'Livros'
+
 return  `Já ${singularOrPluralHa} ${booksBox.booksIn} ${singularOrPluralBooks}`
 
 }
